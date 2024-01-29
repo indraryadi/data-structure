084f58bfbcf1b4c30f9616bdfe6fb6639a5b0cf7
indra appkey <indra.appkey@gmail.com>
2024-01-29 10:54:33 +0800

    create func for graph

--- graph.go
+++ graph.go
@@ -6,15 +6,21 @@ import (
 )
 
 type Vertices struct {
-	Data string
+	Vertices []string
 }
 
 type Graph struct {
 	Matrix [][]int
 }
 
-func printGraph(graph *Graph) {
+func printGraph(vertices *Vertices, graph *Graph) {
+	fmt.Printf(" ")
+	for x := range vertices.Vertices {
+		fmt.Printf(" %s", vertices.Vertices[x])
+	}
+	fmt.Println()
 	for i := range graph.Matrix {
+		fmt.Printf("%s ", vertices.Vertices[i])
 		for j := range graph.Matrix[i] {
 			fmt.Printf(strconv.Itoa(graph.Matrix[i][j]) + " ")
 		}
@@ -22,17 +28,54 @@ func printGraph(graph *Graph) {
 	}
 }
 
+func addVertices(vertices *Vertices, data string) *Vertices {
+	vertices.Vertices = append(vertices.Vertices, data)
+	return &Vertices{
+		Vertices: vertices.Vertices,
+	}
+}
+
+func addEdge(graph *Graph, src int, dst int) {
+	status := checkEdge(graph, src, dst)
+	if status {
+		fmt.Println("cannot add edge")
+	} else {
+		graph.Matrix[src][dst] = 1
+		fmt.Println("done")
+	}
+}
+
+func checkEdge(graph *Graph, src int, dst int) bool {
+	if graph.Matrix[src][dst] == 1 {
+		return true
+	} else {
+		return false
+	}
+}
+
 func main() {
+	row := 4
+	col := 4
+
+	sliceOfVertices := &Vertices{}
+
+	addVertices(sliceOfVertices, "A") //0
+	addVertices(sliceOfVertices, "B") //1
+	addVertices(sliceOfVertices, "C") //2
+	addVertices(sliceOfVertices, "D") //3
+	fmt.Println(sliceOfVertices)
+
 	twoD := &Graph{
-		Matrix: make([][]int, 3),
+		Matrix: make([][]int, row),
 	} //row
 
 	for i := range twoD.Matrix {
-		twoD.Matrix[i] = make([]int, 4, 4) //col
+		twoD.Matrix[i] = make([]int, col) //col
 	}
 
-	twoD.Matrix[1][1] = 3
-
-	printGraph(twoD)
+	fmt.Println(sliceOfVertices.Vertices[1])
+	addEdge(twoD, 0, 1)
+	addEdge(twoD, 0, 1)
+	printGraph(sliceOfVertices, twoD)
 
 }
